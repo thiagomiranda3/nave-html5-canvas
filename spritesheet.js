384dbc7e88040755b7b3a0c776ed44319ecfada1
Thiago Miranda <marine.rlivielor@gmail.com>
2017-07-27 22:47:46 -0300

    Animação com spritesheets

---
+++ spritesheet.js
@@ -0,0 +1,42 @@
+function Spritesheet(context, imagem, linhas, colunas) {
+    this.ctx = context
+    this.imagem = imagem
+    this.numLinhas = linhas
+    this.numColunas = colunas
+    this.intervalo = 0
+    this.linha = 0
+    this.coluna = 0
+    this.ultimoTempo = new Date().getTime()
+}
+
+Spritesheet.prototype.proximoQuadro = function () {
+    let agora = new Date().getTime()
+
+    // verifica se já pode mudar o quadro de acordo com intervalo definido
+    if(agora - this.ultimoTempo < this.intervalo) return
+
+    if(this.coluna < this.numColunas - 1)
+        this.coluna++
+    else
+        this.coluna = 0
+
+    this.ultimoTempo = agora
+}
+
+Spritesheet.prototype.desenhar = function (xDestino, yDestino) {
+    let tamanhoX = this.imagem.width / this.numColunas
+    let tamanhoY = this.imagem.height / this.numLinhas
+    let xOrigem = tamanhoX * this.coluna
+    let yOrigem = tamanhoY * this.linha
+
+    this.ctx.drawImage(this.imagem,
+        xOrigem,
+        yOrigem,
+        tamanhoX,
+        tamanhoY,
+        xDestino,
+        yDestino,
+        tamanhoX,
+        tamanhoY
+    )
+}
